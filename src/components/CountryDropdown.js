57e3865f1291fe71029e6080981d5c583548fce2
mohamed <mohamed.araby288@gmail.com>
2022-11-04 01:58:16 +0200

    Country Dropdown

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
+// import icons 
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri'
+
+
+// import headless ui
+import { Menu } from '@headlessui/react';
+
+// import Context
+import { HouseContext } from './HouseContext';
 const CountryDropdown = () => {
-  return <div>CountryDropdown</div>;
+  const { country, setCountry, countries } = useContext(HouseContext);
+  console.log(countries)
+
+  const [isopen, setIsOpen] = useState(false);
+
+  return <Menu as='div' className='dropdown relative'>
+    <Menu.Button
+      onClick={() => setIsOpen(!isopen)}
+      className='dropdown-btn w-full text-left '>
+      <RiMapPinLine className='dropdown-icon-primary' />
+      <div>
+        <div className='text-[15px] font-medium leading-tight'>{country} </div>
+        <div className='text-[13px]'> Select Your Place </div>
+        {
+          isopen ? (
+            <RiArrowUpSLine
+              className='dropdown-icon-secondary' />
+          ) : (<RiArrowDownSLine
+            className='dropdown-icon-secondary' />)
+        }
+      </div>
+    </Menu.Button>
+    <Menu.Items className='dropdown-menu'>
+      {countries.map((country, index) => {
+        return (
+          <Menu.Item
+          onClick={()=>setCountry(country)}
+            className='cursor-pointer
+           hover:text-violet-700 transition'
+            as='li'
+            key={index}>
+            {country}
+          </Menu.Item>
+        )
+      })}
+    </Menu.Items>
+  </Menu>;
 };
 
 export default CountryDropdown;
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React,{useState,useEffect,createContext} from 'react';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
-};
+// import data
+import {housesData} from '../data'
 
-export default HouseContext;
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({children}) => {
+
+  const [houses,setHouses] = useState(housesData);
+  const [country, setCountry] = useState('location (any)');
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState('Property type (any)');
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState('Price range (any)');
+  const [loading, setLoading] = useState(false);
+  // retun All Countries 
+  useEffect (()=>{
+    const allCountries = houses.map((house)=>{
+      return house.country;
+    },[]);
+    // remove Duplicates
+    const uniqueCountries = [' Location(any)', ...new Set (allCountries)];
+
+    // set countries state
+
+    setCountries(uniqueCountries)
+
+  },[]);
+
+  return ( <HouseContext.Provider value={{
+    country,
+    setCountry,
+    countries,
+    setCountries,
+    property,
+    setProperty,
+    properties,
+    price,
+    setPrice,
+    houses,
+    loading
+  }}>
+  {children}
+  </HouseContext.Provider>
+  );
+}
+
+export default HouseContextProvider;
